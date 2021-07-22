10dc778249331d97fced31e8c265805d379a695b
Mohammed Tarek <mohammedtarek74@yahoo.com>
2021-07-22 02:52:07 +0200

    API is ready to be tested on parts 1 & 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.get('/starts', (req, res) => {
+app.get('/start', (req, res) => {
     const result = getInitialCoordniates();
     res.send(result);
 });
